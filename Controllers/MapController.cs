73ab79c5b7a4c0708ded2390bb61db4046e45003
lukaszpasek <lukasz.pasek7@wp.pl>
2024-01-10 19:58:44 +0100

    Initial commit

---
+++ Controllers/MapController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunPlanner.Controllers
+{
+    public class MapController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+
