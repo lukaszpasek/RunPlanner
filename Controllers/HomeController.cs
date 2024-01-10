8e4d8db64aa903fb694969143d720fe8ce7f0cb0
lukaszpasek <lukasz.pasek7@wp.pl>
2024-01-10 23:10:20 +0100

    add routes in geoJsonData

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace RunPlanner.Controllers
             return View();
         }
 
+        public IActionResult Sample()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
