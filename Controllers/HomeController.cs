d12c00c1a1ebee1c3091ca158ac85bc90022a86f
lukaszpasek <lukasz.pasek7@wp.pl>
2024-01-12 17:14:03 +0100

    HEREMap added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace RunPlanner.Controllers
             return View();
         }
 
+        public IActionResult HEREMap()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
